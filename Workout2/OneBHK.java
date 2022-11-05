1b3f36fa0e657f6320e6c17557efdc35633182a0
itsmeabhishekn <abhishekn4454@gmail.com>
2022-11-06 00:35:31 +0530

    2 programs added , OneBHk program is incomplete though

---
+++ Workout2/OneBHK.java
@@ -0,0 +1,63 @@
+//TODO
+// Create class OneBHK with instance variable roomArea, hallArea and price
+//a. Create a default and parameterized constructor;
+//b. Method show(): to print OneBHK data member information;
+//Create another class TwoBHK which has all the properties and behaviour of OneBHK and a
+//new instance variable room2Area.
+//a. Create a default and parameterized constructors;
+//b. Method show(): to print all data member information;
+//Write the main function in another class (Say DriverClass), store three TwoBHK flat’s
+//information and print information using the show method. Also, print the total amount of all
+//flat
+
+public class OneBHK {
+    double roomArea;
+    double hallArea;
+    double price;
+
+    public OneBHK()
+    {
+        System.out.println("This is the default constructor for OneBHK");
+    }
+
+    public OneBHK(double rmA , double hlA , double prc)
+    {
+        roomArea = rmA;
+        hallArea = hlA;
+        price = prc;
+    }
+
+    public void showOneBHK()
+    {
+        System.out.println("Room area : "+this.roomArea);
+        System.out.println("Hall area : "+this.hallArea);
+        System.out.println("Price     : "+this.price);
+    }
+}
+
+class TwoBHK extends OneBHK
+{
+    double room2Area ;
+
+    public TwoBHK(){
+        System.out.println("This is the default constructor for TwoBHK");
+    }
+    TwoBHK(double r2A)
+    {
+        room2Area =r2A;
+    }
+    public void showTwoBHK()
+    {
+        System.out.println("Room area : "+this.roomArea);
+        System.out.println("Hall area : "+this.hallArea);
+        System.out.println("Price     : "+this.price);
+        System.out.println("Room 2 Are:"+this.room2Area);
+    }
+}
+
+class Driver1 {
+    public static void main(String[] args) {
+        //TwoBHK t1 = new TwoBHK(100,200,140);
+
+    }
+}
